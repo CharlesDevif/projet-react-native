b377feb249b57aa42e1519f390e6b1a310fdb2ea
Charles Devif <77075117+CharlesDevif@users.noreply.github.com>
2023-10-02 14:48:57 +0200

    Co-authored-by: GUILLEMIN Morgan <MorganGui@users.noreply.github.com> Co-authored-by: AlexandreMalvy <AlexandreMalvy@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { useState } from 'react'
-// import { NavigationContainer } from '@react-navigation/native'
-// import { createStackNavigator } from '@react-navigation/stack'
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
-import Login from './views/login'
-import Home from './views/acceuil'
-
-// const Stack = createStackNavigator()
+import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import Login from "./views/login";
+import Register from "./views/register";
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [profile, setProfile] = useState(null)
+  const [profile, setProfile] = useState(null);
 
   return (
-    <View style={styles.container}>
-      <Login />
-    </View>
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Login" component={Login} />
-    //     <Stack.Screen  options={{ headerShown: false }} name="Acceuil" component={Home} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-  )
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen  options={{ headerShown: false }}   name="Login" component={Login} />
+          <Stack.Screen  options={{ headerShown: false }}   name="Register" component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+ 
+  );
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +26,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  }
-})
+    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
