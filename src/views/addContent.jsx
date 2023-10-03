d9e06966e8c271b8fad13469a7d8c5689047409c
MorganGui <morgan.guillemin1@gmail.com>
2023-10-03 16:18:58 +0200

    start of addContentView

--- src/views/addContent.jsx
+++ src/views/addContent.jsx
@@ -1,5 +1,32 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native'
+import { useContext } from 'react'
+import Board from "../components/Board"
+
+
 export default () => {
+    const [name, setName] = useState('')
+    const [owner, setOwner] = useState('')
+
+
+    function addContent() {
+        
+      }
+
   return (
-    <></>
+    <View style={styles.container}>
+      
+      <Button onClick={addContent}>+</Button>
+      
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      gap: 16,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    }
+  })
