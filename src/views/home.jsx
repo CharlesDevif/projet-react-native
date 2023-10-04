6714da20a524224d99c4abbcb00ce8d73683c588
Charles Devif <77075117+CharlesDevif@users.noreply.github.com>
2023-10-04 12:27:07 +0200

    fix menu

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { SafeAreaView, StatusBar, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
 
 import AppContext from '../context'
 import Board from '../classes/Board'
@@ -7,33 +7,34 @@ import AddContentMenu from '../components/addContentMenu'
 
 export default () => {
   const { profile } = useContext(AppContext)
+  const [isMenuModalOpen, setMenuModalOpen] = useState(false)
+
   Board.listenByOwner(profile.id, res => {
     console.log(res)
   })
-
   return (
-    <>
-      <SafeAreaView style={styles.container}>
+    <TouchableWithoutFeedback onPress={() => setMenuModalOpen(!isMenuModalOpen)}>
+      <View style={styles.container}>
         <StatusBar style="light" />
         <Text style={styles.headerListeBoard}>
           { profile ? `Espace de travail de : ${profile.email}` : 'Déconnecté' }
         </Text>
-      </SafeAreaView>
-      <AddContentMenu />
-    </>
+        <AddContentMenu  toggleMenu={isMenuModalOpen} />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    backgroundColor: "#171b1e",
+    width: '100%',
+    backgroundColor: '#171b1e',
   },
   headerListeBoard: {
     padding: 16,
-    color: "#ffff",
-    backgroundColor: "#000000",
-    textAlign: 'center'
-  }
+    color: '#ffff',
+    backgroundColor: '#000000',
+    textAlign: 'center',
+  },
 })
