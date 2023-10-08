095100ffbccf02143389de78deb614dcadb30497
Charles Devif <77075117+CharlesDevif@users.noreply.github.com>
2023-10-08 23:44:56 +0200

    ajout de keys

---
+++ src/keys/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyC0FYARYzCk_ZVsNWHGsnWFfWze6Bi-U1M",
+  authDomain: "lc-react-native-trello.firebaseapp.com",
+  projectId: "lc-react-native-trello",
+  storageBucket: "lc-react-native-trello.appspot.com",
+  messagingSenderId: "144894020354",
+  appId: "1:144894020354:web:6b46aeb4778ceb57ea901b"
+}
